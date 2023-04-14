ccfcd98c3c9956ff17940613d4792ceea08d3e69
praphan <praphanhc@gmail.com>
2023-04-14 10:02:22 +0800

    add in faqs & features

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -1,27 +1,50 @@
 /**
  * GET /
- * Homepage 
-*/
+ * Homepage
+ */
 exports.homepage = async (req, res) => {
   const locals = {
     title: "NodeJs Notes",
     description: "Free NodeJS Notes App.",
-  }
-  res.render('index', {
+  };
+  res.render("index", {
     locals,
-    layout: '../views/layouts/front-page'
+    layout: "../views/layouts/front-page",
   });
-}
-
+};
 
 /**
  * GET /
- * About 
-*/
+ * About
+ */
 exports.about = async (req, res) => {
   const locals = {
     title: "About - NodeJs Notes",
     description: "Free NodeJS Notes App.",
-  }
-  res.render('about', locals);
-}
+  };
+  res.render("about", locals);
+};
+
+/**
+ * GET /
+ * Features
+ */
+exports.features = async (req, res) => {
+  const locals = {
+    title: "About - NodeJs Notes",
+    description: "Free NodeJS Notes App.",
+  };
+  res.render("features", locals);
+};
+
+/**
+ * GET /
+ * FAQs
+ */
+exports.faqs = async (req, res) => {
+  const locals = {
+    title: "About - NodeJs Notes",
+    description: "Free NodeJS Notes App.",
+  };
+  res.render("faqs", locals);
+};
